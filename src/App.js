b8fb82b2531235834e850a57e65663f795d2e195
root <ddphillyfan@gmail.com>
2020-04-10 17:00:00 -0400

    Done. Want to add recovered to line graph and host.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
-
+import coronaImage from './images/image.png';
 
 class App extends React.Component {
     state = {
@@ -11,25 +11,28 @@ class App extends React.Component {
     }
 
     async componentDidMount() {
-        const fetchedData = await fetchData();
+        const data = await fetchData();
 
-        this.setState({ data: fetchedData });
+        this.setState({ data });
     }
 
     handleCountryChange = async (country) => {
-        console.log(country);
-        // fetch the data
-        // set the state
+        const data = await fetchData(country);
+
+        this.setState({ data, country: country });
     }
 
     render() {
-        const { data } = this.state;
+        const { data, country } = this.state;
 
         return (
             <div className={styles.container}>
+                <a href="https://www.cdc.gov/coronavirus/2019-ncov/index.html">
+                    <img className={styles.image} src={coronaImage} alt="COVID-19" />
+                </a>
                 <Cards data={data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange} />
-                <Chart />
+                <Chart data={data} country={country} />
             </div>
         )
     }
--- src/api/index.js
+++ src/api/index.js
@@ -2,14 +2,19 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+    let changeableUrl = url;
+
+    if(country) {
+        changeableUrl = `${url}/countries/${country}`
+    }
 
     try {
-        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
 
         return { confirmed, recovered, deaths, lastUpdate };
-
     } catch(error) {
+        console.log(error)
 
     }
 
@@ -19,11 +24,13 @@ export const fetchDailyData = async () => {
     try {
       const { data } = await axios.get(`${url}/daily`);
 
-
       const modifiedData = data.map((dailyData) => ({
           confirmed: dailyData.confirmed.total,
           deaths: dailyData.deaths.total,
+          recovered: dailyData.recovered.total,
           date: dailyData.reportDate,
+
+          
       }));
 
       return modifiedData;
